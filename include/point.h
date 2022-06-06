299bb29051ebf045a2815049cae48d0576e84de5
chnaha <jch618@naver.com>
2022-06-06 19:58:51 +0900

    fix bug: initializing bug in point and body in snake

--- include/point.h
+++ include/point.h
@@ -2,15 +2,12 @@
 #define POINT_H
 class Point {
 public: // 굳이 캡슐화를 해야할까?
-  Point(int y=0, int x=0) {}
-  int prevX, prevY, y, x;
-  void setPos(const Point& p) { prevY = y; prevX = x; y = p.y; x = p.x; }
-  Point& operator=(const Point& p) { prevY = y; prevX = x; y = p.y; x = p.x; return *this; }
-  // Point(int y, int x): _y(y), _x(x) {}
-  // int getY() { return y; }
-  // int getX() { return x; }
-  // void setY(int y) { _y = y; }
-  // void setX(int x) { _x = x; }
-  // void move
+  Point(int y=0, int x=0): y(y), x(x) {}
+  // int prevX, prevY, y, x;
+  int y, x;
+  void setPos(const Point& p) { y = p.y; x = p.x; }
+  Point& operator=(const Point& p) { y = p.y; x = p.x; return *this; }
 };
+bool operator==(const Point& p1, const Point& p2);
+
 #endif
--- src/point.cpp
+++ src/point.cpp
@@ -1,1 +1,5 @@
 #include "../include/point.h"
+bool operator==(const Point& p1, const Point& p2)
+{
+  return p1.x == p2.x && p1.y == p2.y;
+}
